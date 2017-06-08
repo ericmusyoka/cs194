f147c881a309c1bdcf15ac2503552695661d5ca7
Eric Musyoka <ericmusyoka@DN0a2268b8.SUNet>
2017-06-08 16:56:36 -0700

    crawler done

--- teacherbot/crawler/src/crawler/WikipediaCrawler.java
+++ teacherbot/crawler/src/crawler/WikipediaCrawler.java
@@ -4,34 +4,112 @@ package crawler;
 import java.io.IOException;
 import java.util.*;
 
-import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class WikipediaCrawler {
 	private final String sourceUrl;
+	
+	private int docCount;
 
 	private Queue<String> queue = new LinkedList<String>();
 
 	private List<String> wikipediaData ;
+	private Set<String> history;
 
-	public WikipediaCrawler(String sourceUrl) {
+	public WikipediaCrawler(String sourceUrl, int docCount) {
 		this.sourceUrl = sourceUrl;
+		this.docCount = docCount + 1;
 		queue.offer(sourceUrl);
 		wikipediaData = new ArrayList<String>();
+		history = new HashSet<String>();
 	}
 
 	//Adds a single page to the index
-	public void crawl() throws IOException {
-		String currentUrl = queue.poll();
-		Elements paragraphs = getParagraphs(currentUrl);
-		System.out.println(paragraphs.size());
-		indexParagraphs(paragraphs);
-		// addLinksInParagraphsToQueue(paragraphs);
+	public void crawl() {
+		while(!queue.isEmpty()) {
+			String currentUrl = queue.poll();
+			history.add(currentUrl);
+			try {
+				Elements paragraphs = getParagraphs(currentUrl);
+				indexParagraphs(paragraphs);
+				addLinksInParagraphsToQueue(paragraphs);
+				saveDocument();
+				docCount++;
+				wikipediaData.clear();
+			} catch(IOException ex) {
+				//do nothing
+			}
+	
+		}
+	}
+	
+	public void saveDocument() {
+		String document = "";
+		for(String paragraph: wikipediaData) {
+			document += paragraph;
+		}
+		
+		FileWriter fileWriter = null;
+		BufferedWriter bufferedWriter = null;
+
+		try {
+			fileWriter = new FileWriter("data/" + docCount + ".txt");
+			bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.write(document);
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (bufferedWriter != null) {
+					bufferedWriter.close();
+				}
+				if (fileWriter != null) {
+					fileWriter.close();
+				}
+				
+			} catch(IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
+	public void addLinksInParagraphsToQueue(Elements paragraphs) {
+		for(Element paragraph: paragraphs) {
+			NodeIterable paragraphIterable = new NodeIterable(paragraph);
+			for(Node node: paragraphIterable) {
+				if(node instanceof Element) {
+					Element nodeElement = (Element)node;
+					if(nodeElement.tagName().equals("a")) {
+						String url = node.attr("href");
+						String absUrl = "https://www.wikipedia.org" + url;
+						if(isWikipediaLink(url) && shouldQueue(absUrl)) {
+							queue.add(absUrl);
+							System.out.println(absUrl);
+						}
+					}
+				}
+			}
+			
+		}
+	}
+	
+	private boolean isWikipediaLink(String link) {
+		return link.startsWith("/wiki/");
+	}
+	
+	private boolean shouldQueue(String url) {
+		return !queue.contains(url);
 	}
 
 	//Reads wikipedia page and returns text within <p> tags
@@ -42,12 +120,13 @@ public class WikipediaCrawler {
 	}
 
 	public void processParagraph(Node paragraphNode) {
+		String text = "";
 		for(Node node: new NodeIterable(paragraphNode)) {
 			if (node instanceof TextNode) {
-				wikipediaData.add(((TextNode) node).text());
+				text += ((TextNode) node).text();
 			}
 		}
-
+		wikipediaData.add(text);
 	}
 
 	public void indexParagraphs(Elements paragraphs) {
@@ -61,9 +140,12 @@ public class WikipediaCrawler {
 	}
 
 	public static void main(String[] args) throws IOException {
-		WikipediaCrawler wiki = new WikipediaCrawler("https://en.wikipedia.org/wiki/Egypt");
+		WikipediaCrawler wiki = new WikipediaCrawler("https://en.wikipedia.org/wiki/Portal:History", 0);
 		wiki.crawl();
-		System.out.println(wiki.getData().get(0));
+//		List<String> data = wiki.getData();
+//		for(int i=0; i<data.size(); i++) {
+//			System.out.println(wiki.getData().get(i));	
+//		}
 	}
 
 }
