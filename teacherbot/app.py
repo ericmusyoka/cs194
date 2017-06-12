e00c03d5a30dba5a7f99b47965695892ba75fdcf
Eric Musyoka <ericmusyoka@DN51vtq2.SUNet>
2017-06-11 21:29:46 -0700

    fix

--- teacherbot/app.py
+++ teacherbot/app.py
@@ -4,6 +4,11 @@ from flask import request
 import json
 import requests
 import threading
+
+from TeacherBot import TeacherBot
+
+teacherBot = TeacherBot()
+
 app = Flask(__name__)
 
 def send_message(message, uid, bid):
@@ -26,23 +31,12 @@ def receive_message():
     message = request.json['text']
     uid = request.json['userId']
     bid = request.json['botId']
-    threading.Thread(target=send_message, args=(message, uid, bid,)).start()
+    response = teacherBot.getResponse(message)
+    threading.Thread(target=send_message, args=(response, uid, bid,)).start()
     return jsonify(message='--ACK--')
 
 if __name__ == '__main__':
-    data = {
-        "username": "teacherbot",
-        "password": "teacherbot"
-    }
-    print("Before login.")
-    #"http://protected-ridge-46546.herokuapp.com/admin/botLogin"
-    response = requests.post("http://chatio.ngrok.io/admin/botLogin", json=data)
-    global bot_cookies
-    bot_cookies = response.cookies
-    print("After login.")
     app.run(debug=True)
-
-
 
 
 
